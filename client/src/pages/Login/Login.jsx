665115f35ced979610fdb4269571a06badb12b50
Kapil Adhikari <kapiladhikari2060@gmail.com>
2024-09-18 17:15:55 +0545

    Login page

---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,52 @@
+// Styles
+import "./Login.css";
+
+function Login() {
+    const container = document.getElementById('container')
+  return (
+    <>
+      <div className="loginContainer" id="container">
+        <div className="form-container sign-up">
+          <form>
+            <h1>Login as Teacher</h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+        <div className="form-container sign-in">
+          <form>
+            <h1>Login as Student</h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign In</button>
+          </form>
+        </div>
+        <div className="toggle-container">
+          <div className="toggle">
+            <div className="toggle-panel toggle-left">
+              <h1>Welcome Teachers!!!</h1>
+              <p>Enter your credentials to use all of site features</p>
+              <button className="hidden"onClick={() => {
+                container.classList.remove('active') 
+              }
+              }>Login as Student</button>
+            </div>
+            <div className="toggle-panel toggle-right">
+              <h1>Welcome Students!</h1>
+              <p>
+                Enter your credentials to use all of site features
+              </p>
+              <button className="hidden" onClick={() => {
+                container.classList.add('active')
+              }
+              }>Login as Teacher</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
